c8b18788e226533f811d55ddc893b027c237f9e0
stetsonharding <stetson2001@msn.com>
2022-01-05 18:54:34 -0800

    GraphQL setup.

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
